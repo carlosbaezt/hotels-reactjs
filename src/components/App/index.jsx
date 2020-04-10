1b001cfb38ec7125f1ff3a99dc2bd32ee7eab371
Carlos Baez <cbaez@coredigital.com>
2020-04-10 10:11:04 -0500

    add hotels and hotel component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,6 +1,7 @@
 import Hero from '../Hero';
 import React from 'react';
 import Filters from '../Filters';
+import Hotels from '../Hotels';
 
 class App extends React.Component {
 
@@ -14,8 +15,24 @@ class App extends React.Component {
                 country: undefined,
                 price: undefined,
                 rooms: undefined
-            }
+            },
+            hotels: [],
+            loadingHotels: true
         }
+    }
+    
+    componentDidMount() {
+      this.setState({
+        loadingHotels: true
+      })
+      fetch('https://wt-8a099f3e7c73b2d17f4e018b6cfd6131-0.sandbox.auth0-extend.com/acamica')
+        .then((response) => response.json())
+        .then((hotels) => {
+          setTimeout(() => { this.setState({
+            hotels: hotels,
+            loadingHotels: false
+          })}, 1000)
+        });
     }
 
     handleFilterChange = (filters) => {
@@ -29,6 +46,7 @@ class App extends React.Component {
           <div>
             <Hero filters={ this.state.filters } />
             <Filters filters={ this.state.filters } onFilterChange={ this.handleFilterChange } />
+            <Hotels data={ this.state.hotels } loading={this.state.loadingHotels}/>
           </div>
         )
     }
---
+++ src/components/Hotels/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Hotel from '../Hotel';
+
+class Hotels extends React.Component {
+    render() {
+        return(
+        <section className="section" style={ {marginTop: '3em'} }>
+            <div className="container">
+                <div className="columns is-multiline">
+                { this.props.loading ? (
+                    <progress class="progress is-large is-info" max="100"></progress>
+                ) : (
+                    this.props.data.length ===  0 ? 
+                    (
+                        <article className="message is-warning">
+                            <div className="message-body">
+                                No se han encontrado hoteles que coincidan con los parámetros de búsqueda.
+                            </div>
+                        </article>
+                    ) : (
+                        this.props.data.map((hotel) => (
+                            <div key={hotel.slug} className="column is-one-third">
+                                <Hotel data={ hotel } />
+                            </div>
+                        ))
+                    ))
+                }
+                </div>
+            </div>
+        </section>
+        )
+    }
+}
+
+export default Hotels;
