3cef4bce59575a42ea2e9a5af61a4f07cc89af7f
Carlos Baez <cbaez@coredigital.com>
2020-04-12 13:51:28 -0500

    refactoring and formmating

--- src/components/Hotels/index.jsx
+++ src/components/Hotels/index.jsx
@@ -7,23 +7,26 @@ class Hotels extends React.Component {
         <section className="section" style={ {marginTop: '2em'} }>
             <div className="container">
                 <div className="columns is-multiline">
-                { this.props.loading ? (
-                    <progress className="progress is-large is-info" max="100"></progress>
-                ) : (
-                    this.props.data.length ===  0 ? 
+                {
+                    this.props.loading ? (
+                        <progress className="progress is-large is-info" max="100"></progress>
+                    ) :
                     (
-                        <article className="message is-warning" style={ { width: '100%' } }>
-                            <div className="message-body">
-                                No se han encontrado hoteles que coincidan con los parámetros de búsqueda.
-                            </div>
-                        </article>
-                    ) : (
-                        this.props.data.map((hotel) => (
-                            <div key={hotel.slug} className="column is-one-third">
-                                <Hotel data={ hotel } />
-                            </div>
-                        ))
-                    ))
+                        this.props.data.length ===  0 ? (
+                            <article className="message is-warning" style={ { width: '100%' } }>
+                                <div className="message-body">
+                                    No se han encontrado hoteles que coincidan con los parámetros de búsqueda.
+                                </div>
+                            </article>
+                        ) :
+                        (
+                            this.props.data.map((hotel) => (
+                                <div key={hotel.slug} className="column is-one-third">
+                                    <Hotel data={ hotel } />
+                                </div>
+                            ))
+                        )
+                    )
                 }
                 </div>
             </div>
--- src/components/Price/index.jsx
+++ src/components/Price/index.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
 function Price(props) {
-    const dollars = [];
+    const dollarsIcons = [];
     for (let i = 1; i <= 4; i++) {
         if(i <= props.price) {
-            dollars.push(<i key={`${props.slug}-price-${i}`} className="fas fa-dollar-sign" style={{margin: '0 .125em'}}></i>);
+            dollarsIcons.push(<i key={`${props.slug}-price-${i}`} className="fas fa-dollar-sign" style={{margin: '0 .125em'}}></i>);
         } else {
-            dollars.push(<i key={`${props.slug}-price-${i}`} className="fas fa-dollar-sign" style={{margin: '0 .125em', opacity: '.25'}}></i>);
+            dollarsIcons.push(<i key={`${props.slug}-price-${i}`} className="fas fa-dollar-sign" style={{margin: '0 .125em', opacity: '.25'}}></i>);
         }
     }
 
     return (
         <span className="tag is-medium is-info">
-            {dollars}
+            {dollarsIcons}
         </span>
     )
 }
